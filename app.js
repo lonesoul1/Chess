b42ead3833044642a1eed2f548e845744abc07c4
Divakar Kumar Gupta <monotonicphoton@gmail.com>
2024-07-19 20:55:23 +0530

    socket established on both front end and backend

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
 const express = require("express");
 const socket = require("socket.io");
 const http = require("http");
-const { Chess } = requise("chess.js");
+const { Chess } = require("chess.js");
 const path = require("path");
+const { title } = require("process");
+
+const app=express();
+const server=http.createServer(app);
+const io = socket(server);
+
+const chess = new Chess();
+let players={};
+let currentPlayer="W";
+
+app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get("/",(req,res)=>{
+    res.render("index",{title:"Chess Game"});
+});
+
+io.on("connection",function(uniquesocket){
+    console.log("connected");
+})
+
+server.listen(3000, function (){
+    console.log("listening on port 3000");
+});
