c04dee8cd8786fa27dec51ffeb804e7640cf2b3d
Divakar Kumar Gupta <monotonicphoton@gmail.com>
2024-07-20 16:03:14 +0530

    backend setup complete

--- app.js
+++ app.js
@@ -22,7 +22,48 @@ app.get("/",(req,res)=>{
 
 io.on("connection",function(uniquesocket){
     console.log("connected");
+    
+    if(!players.white){
+        players.white=uniquesocket.id;
+        uniquesocket.emit("playerRole","w");
+    }else if(!players.black){
+        players.black=uniquesocket.id;
+        uniquesocket.emit("playerRole","b");
+    }else{
+        uniquesocket.emit("spectatorRole");
+    }
+    
+    uniquesocket.on("disconnect",function(){
+        if(uniquesocket.id===players.white){
+            delete players.white;
+        }else{
+            delete players.black;
+        }
+    })
+});
+
+uniquesocket.on("move",(move)=>{
+    try{
+        if(chess.turn()=="w" && uniquesocket.id!=players.white) return;
+        if(chess.turn()=="b" && uniquesocket.id!=players.black) return;
+
+        const result = chess.move(move);
+        if(result){
+            currentPlayer=chess.turn();
+            io.emit("move",move);
+            io.emit("boardstate",chess.fen());
+        }else{
+            console.log("Invalid move : ", move);
+            uniquesocket.emit("Invalid move : ",move);
+        }
+    }catch(err){//engine fails to process the move 
+        console.log(err);
+        uniquesocket.emit("Invalid move : ",move);
+    }
 })
+
+
+
 
 server.listen(3000, function (){
     console.log("listening on port 3000");
